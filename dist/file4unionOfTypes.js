0d6e59da150ca6016293534bc4626ed3bdd7c5e7
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2023-12-13 16:30:55 +0500

    union of types conversion

---
+++ dist/file4unionOfTypes.js
@@ -0,0 +1,5 @@
+"use strict";
+let numOrString = "this is a string";
+console.log(numOrString);
+numOrString = 123
+console.log(numOrString);
---
+++ src/file4unionOfTypes.ts
@@ -0,0 +1,2 @@
+let numOrString : number | string = "this is a string"
+console.log(numOrString)
