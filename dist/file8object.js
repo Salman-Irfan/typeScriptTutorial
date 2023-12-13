7a282aa4e03d6251f2b8916313675ea329c91ae5
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2023-12-13 17:41:00 +0500

    with with types

---
+++ dist/file8object.js
@@ -0,0 +1,6 @@
+"use strict";
+let salman = {
+    height: 6,
+    weight: 70,
+    gender: "male"
+};
---
+++ src/file8object.ts
@@ -0,0 +1,11 @@
+type Person = {
+    height: number,
+    weight: number,
+    gender: string
+}
+
+let salman: Person = {
+    height: 6,
+    weight: 70,
+    gender: "male"
+}
