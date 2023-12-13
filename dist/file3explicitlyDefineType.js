25dfdcf36890a3a57f8ef0e7d552ec38c694f97a
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2023-12-13 15:57:35 +0500

    single type conversion

---
+++ dist/file3explicitlyDefineType.js
@@ -0,0 +1,8 @@
+"use strict";
+let str = "salman";
+console.log(str);
+let isBool;
+isBool = false;
+let any;
+// or
+let anyDefined;
---
+++ src/file3explicitlyDefineType.ts
@@ -0,0 +1,9 @@
+let str:string = "salman"
+console.log(str)
+
+let isBool:boolean 
+isBool=false
+
+let any
+// or
+let anyDefined:any
