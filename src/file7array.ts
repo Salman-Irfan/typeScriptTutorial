f8567ca9ceaf3a327592f5d36f2bae282fae7afa
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2023-12-13 17:33:37 +0500

    array with types

--- src/file7array.ts
+++ src/file7array.ts
@@ -2,4 +2,4 @@ const arr:string[] = []; // not good
 
 const arr2:Array<string> = []; // good
 
-const arr3:Array<string | number> = [123, "salman"]; // good
+const arr3:Array<string | number> = [123, "salman"]; // good // good
