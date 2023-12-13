e0745588c19884c567c74d89671fb5d1c5758cd6
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2023-12-13 16:51:53 +0500

    type keywords

---
+++ dist/file6typeKeyword.js
@@ -0,0 +1,4 @@
+"use strict";
+let user = "salman"; // valid -> string
+user = 7; // valid -> number
+// user = true // in valid -> boolean
---
+++ src/file6typeKeyword.ts
@@ -0,0 +1,5 @@
+type UserName = string | number
+
+let user:UserName = "salman" // valid -> string
+user = 7 // valid -> number
+// user = true // in valid -> boolean
