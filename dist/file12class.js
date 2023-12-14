c9647fb14e0e74588d82243706ce7bca155c98bf
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2023-12-14 10:33:23 +0500

    class with access modifiers and inheritance

--- dist/file12class.js
+++ dist/file12class.js
@@ -1,15 +1,32 @@
 "use strict";
 class Player {
-    constructor(height, weight) {
-        this.myHeight = () => {
-            return this.height;
-        };
+    // private height;
+    // weight;
+    // protected power?: number;
+    constructor(height, weight, power) {
         this.height = height;
         this.weight = weight;
+        this.power = power;
+        this.getMyHeight = () => {
+            return this.height;
+        };
     }
 }
 // instance of Player class
 const player1 = new Player(6, 70);
 player1.weight = 100;
 console.log(player1.weight);
-console.log(player1.myHeight());
+console.log(player1.getMyHeight());
+// inheritance / protected
+class SpecialPlayer extends Player {
+    constructor(height, weight, power, special) {
+        super(height, weight, power);
+        // accessing protected properties
+        this.getMyPower = () => {
+            return this.power;
+        };
+        this.special = special;
+    }
+}
+const specialPlayer2 = new SpecialPlayer(7, 80, 9999, true);
+console.log(specialPlayer2.getMyPower());
--- src/file12class.ts
+++ src/file12class.ts
@@ -1,16 +1,34 @@
 class Player {
-    private height;
-    weight;
-    constructor(height: number, weight: number){
-        this.height = height;
-        this.weight = weight;
-    }
-    myHeight = () => {
-        return this.height
-    }
+    // private height;
+    // weight;
+    // protected power?: number;
+    constructor(
+        private height: number,
+        public weight: number,
+        protected power?: number
+    ) {}
+    getMyHeight = () => {
+        return this.height;
+    };
 }
 // instance of Player class
-const player1 = new Player(6,70)
-player1.weight = 100
-console.log(player1.weight)
-console.log(player1.myHeight())
+const player1 = new Player(6, 70);
+player1.weight = 100;
+console.log(player1.weight);
+console.log(player1.getMyHeight());
+
+// inheritance / protected
+class SpecialPlayer extends Player {
+    special: boolean;
+    constructor(height:number, weight:number , power: number, special: boolean){
+        super(height, weight, power)
+        this.special = special
+    }
+    // accessing protected properties
+    getMyPower=()=>{
+        return this.power
+    }
+}
+
+const specialPlayer2 = new SpecialPlayer(7,80, 9999, true)
+console.log(specialPlayer2.getMyPower())
